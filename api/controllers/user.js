cd0b69553b4d85ed6d252f1aceeb1c61a8a6d4d5
soleilyasmina <mounlasolomon@gmail.com>
2021-07-12 12:13:20 -0400

    create user profiles on model, routes, and controllers

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -18,7 +18,7 @@ const login = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    const user = await User.create({ ...req.body, password_digest: req.body.password });
+    const user = await User.create({ ...req.body, password_digest: req.body.password, profile: ["newest-found"] });
     const userInfo = extractUserPayload(user);
     const token = createToken(userInfo);
     res.status(201).json({ user: userInfo, token });
@@ -36,8 +36,41 @@ const verify = async (req, res) => {
   }
 };
 
+const updateProfile = async (req, res) => {
+  const { action, option } = req.body;
+  const profileOptions = ["newest-found", "recent", "newest-archived", "featured", "newest-applied", "stale"];
+  const query = { username: res.locals.user.username };
+  if (!action || !option) {
+    res.status(400).json({ error: "Missing parameters!" });
+  } else if (!profileOptions.includes(option)) {
+    res.status(400).json({ error: "Option does not exist." });
+  } else if (action === "add" && res.locals.user.profile.includes(option)) {
+    res.status(400).json({ error: "Dashboard option already enabled." });
+  } else if (action === "remove" && !res.locals.user.profile.includes(option)) {
+    res.status(400).json({ error: "Dashboard option not enabled." });
+  } else if (action === "add") {
+    const newProfile = { profile: [...res.locals.user.profile, option] };
+    await User.findOneAndUpdate(query, newProfile, { new: true, runValidators: true }, (err, user) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else {
+        res.status(200).json({ user: extractUserPayload(user) });
+      }
+    });
+  } else {
+    const newProfile = { profile: res.locals.user.profile.filter((p) => p !== option) };
+    await User.findOneAndUpdate(query, newProfile, { new: true, runValidators: true }, (err, user) => {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      return res.status(200).json({ user: extractUserPayload(user) });
+    });
+  }
+};
+
 module.exports = {
   login,
   register,
   verify,
+  updateProfile,
 };
