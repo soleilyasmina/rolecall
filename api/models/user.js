2205d1ec3a22352186e9a7b20c4b3c8fc7fce4b7
soleilyasmina <mounlasolomon@gmail.com>
2021-07-12 10:17:10 -0400

    create user model

---
+++ api/models/user.js
@@ -0,0 +1,39 @@
+const { model, Schema } = require("mongoose");
+const { hashSync } = require("bcrypt");
+
+const { SALT_ROUNDS } = process.env;
+const salts = parseInt(SALT_ROUNDS, 10);
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    min: [4, "Username is too short!"],
+    max: [16, "Username is too long!"],
+  },
+  password_digest: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (email) => /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email),
+      message: "Email validation failed!",
+    },
+  },
+});
+
+userSchema.pre("save", function hash(next) {
+  if (!this.isModified("password_digest")) return next();
+  this.password_digest = hashSync(this.password_digest, salts);
+  return next();
+});
+
+userSchema.pre("findOneAndUpdate", async function hash() {
+  const doc = await this.model.findOne(this.getQuery());
+  if (this._update.password_digest && doc.password_digest !== this._update.password_digest) {
+    this._update.password_digest = hashSync(this._update.password_digest, salts);
+  }
+});
+
+module.exports = model("users", userSchema);
