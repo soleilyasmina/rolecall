9d006423e497c5675801a9c514e33891ade800be
zulays <zscottborgh@gmail.com>
2021-07-12 12:16:37 -0400

    set up routes for roleControllers. awaiting testing

--- api/routes/role.js
+++ api/routes/role.js
@@ -4,10 +4,9 @@ const { restrict } = require('../utils');
 
 const roleRouter = new Router();
 
-roleRouter.post('/dashboard', restrict, roleController.createRole);
-roleRouter.put('/dashboard', restrict, roleController.getAllRoles);
-roleRouter.delete('/dashboard', restrict, roleController.getAllRoles);
-roleRouter.get('/dashboard', restrict, roleController.getAllRoles);
-roleRouter.get('/dashboard', restrict, roleController.getAllRoles);
+roleRouter.post('/roles', restrict, roleController.createRole);
+roleRouter.put('/roles/id', restrict, roleController.updateRoles);
+roleRouter.delete('/roles/id', restrict, roleController.deleteRoles);
+roleRouter.get('/roles', restrict, roleController.getAllRoles);
 
 module.exports = roleRouter;
