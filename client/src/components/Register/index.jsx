03cba26601f90943ab42fcd8d92a599f42a09a13
zulays <zscottborgh@gmail.com>
2021-07-12 22:10:52 -0400

    login form and component setup and tested

---
+++ client/src/components/Register/index.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Context } from 'context';
+
+const Register = () => {
+  return <></>;
+};
+
+export default Register;
