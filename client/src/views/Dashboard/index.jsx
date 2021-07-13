9ea21037825b3c86832e0e6aee8d8c527958a221
zulays <zscottborgh@gmail.com>
2021-07-13 00:02:14 -0400

    initialize getAllRoles request

--- client/src/views/Dashboard/index.jsx
+++ client/src/views/Dashboard/index.jsx
@@ -1,9 +1,14 @@
 import Card from 'components/Card';
 import Section from 'components/Section';
+import { getRoles } from 'services';
 import { Box } from 'grommet';
 
 const Dashboard = ({ role }) => {
   //getAllRoles
+
+  const getAllRoles = () => {
+    await getRoles();
+  };
   //map through roles to display cards
   //
   return (
