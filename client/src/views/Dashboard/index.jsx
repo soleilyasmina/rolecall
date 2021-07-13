e7864b1a2cb9cef1456129b647e37f274bc8380f
zulays <zscottborgh@gmail.com>
2021-07-12 23:40:30 -0400

    create dashboard layout. import dashboard into App

---
+++ client/src/views/Dashboard/index.jsx
@@ -0,0 +1,38 @@
+import Card from 'components/Card';
+import { Box } from 'grommet';
+
+const Dashboard = ({ role }) => {
+  return (
+    <>
+      <Box
+        direction="column"
+        border={{ color: 'neutral-3', size: 'large' }}
+        pad="small"
+      >
+        <Box
+          direction="row"
+          border={{ color: 'accent-2', size: 'medium' }}
+          pad="medium"
+          margin="small"
+          overflow="scroll"
+        ></Box>
+        <Box
+          direction="row"
+          border={{ color: 'accent-4', size: 'medium' }}
+          pad="medium"
+          margin="small"
+          overflow="scroll"
+        ></Box>
+        <Box
+          direction="row"
+          border={{ color: 'accent-3', size: 'medium' }}
+          pad="medium"
+          margin="small"
+          overflow="scroll"
+        ></Box>
+      </Box>
+    </>
+  );
+};
+
+export default Dashboard;
