c47ba68121ecf9bb91c7289d4e79dccf78e8b322
zulays <zscottborgh@gmail.com>
2021-10-27 19:51:11 -0400

    update controller profileOptions to array of values instead of hard coded value. rubber ducked with Pete to figure out

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,18 @@
-const User = require("../models/user");
-const { createToken, extractUserPayload, comparePasswords } = require("../utils");
+const User = require('../models/user');
+const {
+  createToken,
+  extractUserPayload,
+  comparePasswords,
+} = require('../utils');
+
+const profileOptions = [
+  'newest-found',
+  'newest',
+  'newest-archived',
+  'last-updated',
+  'newest-applied',
+  'stale',
+];
 
 const login = async (req, res) => {
   try {
@@ -9,7 +22,7 @@ const login = async (req, res) => {
       const token = createToken(userInfo);
       res.status(201).json({ user: userInfo, token });
     } else {
-      res.status(401).json({ error: "Not authorized" });
+      res.status(401).json({ error: 'Not authorized' });
     }
   } catch (e) {
     res.status(500).json({ error: e.message });
@@ -18,7 +31,11 @@ const login = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    const user = await User.create({ ...req.body, password_digest: req.body.password, profile: ["newest-found"] });
+    const user = await User.create({
+      ...req.body,
+      password_digest: req.body.password,
+      profile: profileOptions,
+    });
     const userInfo = extractUserPayload(user);
     const token = createToken(userInfo);
     res.status(201).json({ user: userInfo, token });
@@ -38,33 +55,44 @@ const verify = async (req, res) => {
 
 const updateProfile = async (req, res) => {
   const { action, option } = req.body;
-  const profileOptions = ["newest-found", "newest", "newest-archived", "last-updated", "newest-applied", "stale"];
   const query = { username: res.locals.user.username };
   if (!action || !option) {
-    res.status(400).json({ error: "Missing parameters!" });
+    res.status(400).json({ error: 'Missing parameters!' });
   } else if (!profileOptions.includes(option)) {
-    res.status(400).json({ error: "Option does not exist." });
-  } else if (action === "add" && res.locals.user.profile.includes(option)) {
-    res.status(400).json({ error: "Dashboard option already enabled." });
-  } else if (action === "remove" && !res.locals.user.profile.includes(option)) {
-    res.status(400).json({ error: "Dashboard option not enabled." });
-  } else if (action === "add") {
+    res.status(400).json({ error: 'Option does not exist.' });
+  } else if (action === 'add' && res.locals.user.profile.includes(option)) {
+    res.status(400).json({ error: 'Dashboard option already enabled.' });
+  } else if (action === 'remove' && !res.locals.user.profile.includes(option)) {
+    res.status(400).json({ error: 'Dashboard option not enabled.' });
+  } else if (action === 'add') {
     const newProfile = { profile: [...res.locals.user.profile, option] };
-    await User.findOneAndUpdate(query, newProfile, { new: true, runValidators: true }, (err, user) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-      } else {
-        res.status(200).json({ user: extractUserPayload(user) });
+    await User.findOneAndUpdate(
+      query,
+      newProfile,
+      { new: true, runValidators: true },
+      (err, user) => {
+        if (err) {
+          res.status(400).json({ error: err.message });
+        } else {
+          res.status(200).json({ user: extractUserPayload(user) });
+        }
       }
-    });
+    );
   } else {
-    const newProfile = { profile: res.locals.user.profile.filter((p) => p !== option) };
-    await User.findOneAndUpdate(query, newProfile, { new: true, runValidators: true }, (err, user) => {
-      if (err) {
-        return res.status(400).json({ error: err.message });
+    const newProfile = {
+      profile: res.locals.user.profile.filter((p) => p !== option),
+    };
+    await User.findOneAndUpdate(
+      query,
+      newProfile,
+      { new: true, runValidators: true },
+      (err, user) => {
+        if (err) {
+          return res.status(400).json({ error: err.message });
+        }
+        return res.status(200).json({ user: extractUserPayload(user) });
       }
-      return res.status(200).json({ user: extractUserPayload(user) });
-    });
+    );
   }
 };
 
